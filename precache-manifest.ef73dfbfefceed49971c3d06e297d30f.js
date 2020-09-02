ddd88fc86e7c6133dc2986975227151cb8e6a5c0
sokolovdmytro <sokolov.dmytro.an@gmail.com>
2020-09-02 16:55:26 +0300

    Updates

---
+++ precache-manifest.ef73dfbfefceed49971c3d06e297d30f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40b498aaef95d2d32487e0876e400bec",
+    "url": "/goit-react-hw-5-phonebook/index.html"
+  },
+  {
+    "revision": "8ca540bc344c50b8613c",
+    "url": "/goit-react-hw-5-phonebook/static/css/main.b71290e7.chunk.css"
+  },
+  {
+    "revision": "d20346cd230fc299779a",
+    "url": "/goit-react-hw-5-phonebook/static/js/2.b00f375b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-5-phonebook/static/js/2.b00f375b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ca540bc344c50b8613c",
+    "url": "/goit-react-hw-5-phonebook/static/js/main.37c81147.chunk.js"
+  },
+  {
+    "revision": "29765238a485dffc9f60",
+    "url": "/goit-react-hw-5-phonebook/static/js/runtime-main.00d6d3b1.js"
+  }
+]);
